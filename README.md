28474328cb8d8120bfe000eee37569ddef67d528
Femi Da Costa <femi.dacosta@proton.me>
2023-01-29 18:23:08 +0000

    changes to layout

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # about-me
+
+_About me project_
+
+I have enjoyed the about me project. I found the JS challenging but my understanding of `for ` loops, `while` loops and `array` has improved.
+
+I have enjoyed the CSS aspect of the project.
+
+The current score of my about me page it _76%_ tested with Lighthouse.
+
+I can ensure a higher score by making these changes;
+
+- Background and foreground colors do not have a sufficient contrast ratio. Make sure the colors are contrasted for people who have impaired vision.
+
+- Adding Alt tags to any image including the logo.
+
+- Custom controls have associated labels ( as the nav bar needs to be completed!)
+
+<img src="about me/lighthouse 1.png" class="image"/>
--- app.js
+++ app.js
@@ -1,115 +1,83 @@
 let userPoints = 0;
-
 alert("Welcome to my guessing game.");
-
 let user = prompt("What can I call you?");
-
 // ensure the user ACTUALLY gives their name
 while (!user) {
-  user = prompt(
-    "For us to proceed with this important page, I need your name :) "
+  user = prompt("For us to proceed with this page, I need your name :) ");
+  alert("Welcome " + user + "! I'm so glad your here! Lets run the game");
+  console.log("Hello " + user);
+}
+// question one
+function question1() {
+  let location = prompt[("spain", "scotland", "india", "brazil")];
+  let correctAnswer = true;
+  while (!correctAnswer) {
+    let question1 = prompt("What is my favourite location?").toLowerCase();
+    if (place == travelQuestion) {
+      correctAnswer = true;
+      return "Correct!" + travelQuestion + "!";
+      correctAnswer = true;
+      break;
+      userPoints++;
+    } else {
+      alert("try again");
+    }
+  }
+  console.log(travelQuestion + question1);
+}
+// question two
+function question2() {
+  confirm("do you like plants?");
+  if (question2 == false) {
+    alert("Thats cool, I have alot of house plants");
+    userPoints++;
+  } else {
+    alert("Keep going");
+  }
+}
+function question3() {}
+"what is my favourite genre of music?".toLowerCase();
+let genre =
+  prompt[("electroinic", "dub", "reggae", "punk", "tek", "rock", "hiphop")];
+let attempts = 4;
+while (attempts > 0) {
+  for (let i = 0; i < genre.length; i++)
+    if (question3 == genre[i]) {
+      console.log("Correct, I love " + question3 + "!");
+      attempts = 0;
+      userPoints++;
+    }
+  if (attempts > 0) {
+    question3 = prompt("Incorrect, Try Again");
+  }
+}
+attempts--;
+//question four
+function question4() {
+  confirm("Do I like to cook?");
+  if (question4 == true) {
+    console.log("Correct! I love to cook different types of food!");
+    alert("Correct!");
+    userPoints++;
+  } else {
+    alert("Next!!");
+  }
+}
+//question five
+function question5() {
+  ("Am I 30 yet?");
+  if (question5 == true) {
+    console.log("I dont know where the past 10 years have gone!!");
+    userPoints++;
+  } else {
+    alert("wise with age?!");
+  }
+  console.log(
+    " Yes" +
+      question5 +
+      "I turned 30 last month, you have gained " +
+      userPoints +
+      "!"
   );
 }
-
-alert("Welcome " + user + "! I'm so glad your here! Lets run the game");
-
-// question one
-let travelQuestion = prompt("What is my favourite location?").toLowerCase();
-
-let PlacesILike = ["spain", "scotland", "india", "brazil"];
-
-for (let i = 0; i < PlacesILike.length; i++) {
-  if (travelQuestion == PlacesILike[i]) {
-    alert(
-      "Correct, I think I have started to annoy my friends with the amount I talk about " +
-        travelQuestion +
-        "!"
-    );
-    userPoints++;
-  }
-}
-
-// question two
-let plantsQuestion = confirm("do you like plants?");
-
-if (plantsQuestion == false) {
-  alert("Thats cool, I have alot of house plants");
-  userPoints++;
-} else {
-  alert("I have alot of house plants?");
-}
-
-//question three
-let musicQuestion = prompt("what is my favourite genre?").toLowerCase();
-
-let genre = [
-  "electroinic",
-  "dub",
-  "reggae",
-  "punk",
-  "tek",
-  "ambient",
-  "hiphop",
-];
-
-for (let i = 0; i < genre.length; i++) {
-  if (musicQuestion == genre[i]) {
-    alert("Correct, I love " + musicQuestion + "!");
-    userPoints++;
-  }
-}
-
-//question four
-let girlfriend = { herName: "mollie", age: "27", occupation: "biologist" };
-
-console.log("girlfriend" in herName);
-// Expected output: true
-
-delete HerName;
-if ("age" in HerName === false) {
-  herName = "Mollie";
-}
-
-console.log(herName.occupation);
-// Expected output: "Mollie"
-
-//question five
-let carQuestion = prompt("What car do I want to drive?").toLowerCase();
-
-let CarsILike = ["golf", "honda", "bmw", "classic car"];
-
-for (let i = 0; i < CarsILike.length; i++) {
-  if (carQuestion == CarsILike[i]) {
-    alert(
-      "Correct, but im not really that bothered about cars to be honest!" +
-        CarsQuestion +
-        "!"
-    );
-    userPoints++;
-  }
-}
-
-//question six
-let chocolate = [
-  "dairy Milk",
-  "crunchie",
-  "lindor",
-  "galaxy",
-  "dark chocolate",
-];
-
-for (let i = 0; i < chocolate.length; i++) {
-  console.log[i];
-}
-
-let number = 4;
-while (number != 1) {
-  console.log(number);
-  number--; //3
-}
-
-alert("My favourite chocolate is" + chocolate);
-
-alert(
-  "Congratulations my friend " + user + "! You scored " + userPoints + "/6"
-);
+alert("well done " + user + "overall, You scored " + userPoints + "/5");
